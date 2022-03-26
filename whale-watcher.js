29551d2e34e6b9813887809f300dc2607283888a
Jason D'Souza <jason.m.dsouza@gmail.com>
2022-03-26 16:54:14 +1100

    Initial code drop
    Initial code to go to etherscan and get eth balances

---
+++ whale-watcher.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+let etherscan_apikey = '<insert api key>';
+
+const etherscan = axios.create({
+    baseURL: 'https://api.etherscan.io/api'
+});
+
+let watched_addresses_array = ['add addresses'];
+watched_addresses = watched_addresses_array.join(',');
+
+// Construct etherscan api request
+let getAccountBalances = '?module=account&action=balancemulti&address='+watched_addresses+'&tag=latest&apikey='+etherscan_apikey;
+
+// Get watched account eth balances
+etherscan.get(getAccountBalances).then(resp => {
+    console.log(resp.data.result[0].balance);
+});
